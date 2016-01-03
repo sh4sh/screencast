6594c694153509a00faf9e9f81ed191839cf7387
sunny <p@lur.com>
2016-01-03 02:35:45 -0500

    So much changes, not compiling atm

--- screencast.sh
+++ screencast.sh
@@ -1,4 +1,41 @@
 #!/bin/bash
+
+maincast () {
+ recordmydesktop --overwrite --on-the-fly-encoding sc.ogv &
+ castnow --address 192.168.1.5 --tomp4 sc.ogv
+}
+
+bedroomcast () {
+ recordmydesktop --overwrite --on-the-fly-encoding sc.ogv &
+ castnow --address 192.168.1.10 --tomp4 sc.ogv
+}
+
+customaddr () {
+ recordmydesktop --overwrite --on-the-fly-encoding sc.ogv & castnow sc.ogv --address $REPLY
+}
 	
-recordmydesktop --on-the-fly-encoding &;
-castnow out.ogv --address 192.168.1.5 --tomp4
+echo "Which device should I connect to?"
+echo "View Favourites, or enter a custom IP address for your device?"
+select choice in "Favourites" "Custom" ; do
+ case $CHOICE in
+  "Favourites")
+    select device in "Main" "Bedroom" ; do
+     case $DEV in
+      "Main")
+       maincast
+       break
+      ;;
+      "Bedroom")
+       bedroomcast
+       break
+      ;;
+     esac
+    done
+  "Custom")
+   echo "What is the IP address of the device I should connect to?"
+   read -r
+   customaddr $REPLY
+   break
+  ;;
+ esac
+done
