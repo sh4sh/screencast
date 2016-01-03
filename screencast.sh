5fe505ea00801e57cf1fc304c208c945d86c15d9
sunny <p@lur.com>
2016-01-03 00:52:32 -0500

    Removed commenting and cleaned up castnow options

--- screencast.sh
+++ screencast.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#change
 	
-#recordmydesktop --on-the-fly-encoding &;
-#castnow out.ogv --address 192.168.1.5 --tomp4 out.ogv
+recordmydesktop --on-the-fly-encoding &;
+castnow out.ogv --address 192.168.1.5 --tomp4
